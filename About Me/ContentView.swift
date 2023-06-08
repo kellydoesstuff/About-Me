7d0c11a7a659222bf95c65951f6d04396c5a93ab
Kelly Bian <kellybian85@gmail.com>
2023-06-08 13:21:46 -0500

    Initial Commit

---
+++ About Me/ContentView.swift
@@ -0,0 +1,79 @@
+//
+//  ContentView.swift
+//  About Me
+//
+//  Created by Scholar on 6/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var index = 0
+    @State private var facts = ""
+    @State private var image = "kelly"
+//    @State private var trigger = false
+    var body: some View {
+        ZStack{
+           
+            Color("sageGreen")
+                .ignoresSafeArea()
+            
+            VStack (alignment: .center, spacing: 25){
+                Text("Hi, my name is Kelly 👨‍🌾.")
+                    .font(.title)
+
+                Image(image)
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(40)
+                    .frame(width: 345.0, height: 345.0)
+
+                Button("Click here for a fun fact!"){
+                    index = index + 1
+                    if (index == 0){
+                        image = "kelly"
+                    }
+                    else if (index == 1){
+                        image = "robot"
+                        facts = "I do robotics! 🤖"
+                    } else if (index == 2){
+                        image = "guitar"
+                        facts = "I play guitar 🎸."
+                    } else if (index == 3){
+                        image = "kwk"
+                        facts = "This is my second year at KWK! 👩‍💻"
+                    } else if (index == 4){
+                        image = "fifteen"
+                        facts = "I am 15."
+                    } else if (index == 5 ){
+                        image = "china"
+                        index = 0
+                        facts = "I am Chinese 🇨🇳."
+                    }
+
+                }
+                .buttonStyle(.borderedProminent)
+    //            .tint()
+                .padding()
+            
+                Text(facts)
+            }
+            
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(Color("offWhite")))
+            .cornerRadius(40)
+            .shadow(radius: 10)
+            .padding(.vertical, 30)
+            
+            
+        }
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
